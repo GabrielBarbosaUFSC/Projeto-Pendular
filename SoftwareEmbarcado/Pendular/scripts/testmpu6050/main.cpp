df51836eda370724c48fa0306e05ce3de3cba93f
Leo F <leonardoaguiarforner@gmail.com>
2023-05-15 20:16:12 -0300

    sem erros de copilação

--- SoftwareEmbarcado/Pendular/scripts/testmpu6050/main.cpp
+++ SoftwareEmbarcado/Pendular/scripts/testmpu6050/main.cpp
@@ -1,8 +1,53 @@
-#include <Arduino.h>
-void setup(){
+#include "SoftwareWire.h" //Biblioteca para comunicação I2C
 
+struct dados
+{
+  int16_t X;
+  int16_t Y;
+  int16_t Z;
+} acc, gyr;
+
+const int MPU_addr = 0x68; // Endereço do sensor
+
+// int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ; //Variaveis para pegar os valores medidos
+
+void setup()
+{
+  Wire.begin();                     // Inicia a comunicação I2C
+  Wire.beginTransmission(MPU_addr); // Começa a transmissao de dados para o sensor
+  Wire.write(0x6B);                 // registrador PWR_MGMT_1
+  Wire.write(0);                    // Manda 0 e "acorda" o MPU 6050
+  Wire.endTransmission(true);
+
+  Serial.begin(9600); // Inicia a comunicaçao serial (para exibir os valores lidos)
 }
-void loop(){
+void loop()
+{
+  Wire.beginTransmission(MPU_addr); // Começa a transmissao de dados para o sensor
+  Wire.write(0x3B);                 // registrador dos dados medidos (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr, 14, true);     // faz um "pedido" para ler 14 registradores, que serão os registrados com os dados medidos
+  acc.X = Wire.read() << 8 | Wire.read(); // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
+  acc.Y = Wire.read() << 8 | Wire.read(); // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  acc.Z = Wire.read() << 8 | Wire.read(); // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  // Tmp=Wire.read()<<8|Wire.read(); // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  gyr.X = Wire.read() << 16 | Wire.read(); // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  gyr.Y = Wire.read() << 8 | Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+  gyr.Z = Wire.read() << 8 | Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
 
+  // Agora escreve os valores no monitor serial
+  Serial.print("AcX = ");
+  Serial.print(acc.X);
+  Serial.print(" | AcY = ");
+  Serial.print(acc.Y);
+  Serial.print(" | AcZ = ");
+  Serial.print(acc.Z);
+  // Serial.print(" | Tmp = "); Serial.print(Tmp/340.00+36.53); //Equação da temperatura em Cº de acordo com o datasheet
+  Serial.print(" | GyX = ");
+  Serial.print(gyr.X);
+  Serial.print(" | GyY = ");
+  Serial.print(gyr.Y);
+  Serial.print(" | GyZ = ");
+  Serial.println(gyr.Z);
+  delay(333);
 }
-
