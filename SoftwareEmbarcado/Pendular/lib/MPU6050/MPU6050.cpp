556fad7d8b0eae1bd619b82bd3b48e914f48b6a3
Leo F <leonardoaguiarforner@gmail.com>
2023-05-18 05:27:45 -0300

    criação da lib MPU6050

---
+++ SoftwareEmbarcado/Pendular/lib/MPU6050/MPU6050.cpp
@@ -0,0 +1,31 @@
+#include "MPU6050.h"
+
+void MPU6050::begin() {
+  Wire.begin();
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x6B);
+  Wire.write(0);
+  Wire.endTransmission(true);
+}
+
+void MPU6050::readData() {
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x3B);
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr, 14, true);
+  acc.X = Wire.read() << 8 | Wire.read();
+  acc.Y = Wire.read() << 8 | Wire.read();
+  acc.Z = Wire.read() << 8 | Wire.read();
+  Tmp = Wire.read() << 8 | Wire.read();
+  gyr.X = Wire.read() << 8 | Wire.read();
+  gyr.Y = Wire.read() << 8 | Wire.read();
+  gyr.Z = Wire.read() << 8 | Wire.read();
+}
+
+dados MPU6050::getAcceleration() {
+  return acc;
+}
+
+dados MPU6050::getGyroscope() {
+  return gyr;
+}
