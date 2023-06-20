6d4d90c12d956defee9b0bd7619dc6bcbca75538
GabrielBarbosaUFSC <gabriel.a.barbosa@grad.ufsc.br>
2023-06-20 19:25:11 -0300

    add peso componentes

--- AnaliseDados/motor3/motor3.jl
+++ AnaliseDados/motor3/motor3.jl
@@ -8,6 +8,15 @@ using Plots; plotly()
 df = CSV.read(filepath, DataFrame)
 
 voltage_input = @. 17 - 17*df[:,3]/4095
+time = @. df[:,1]/1e6
+speed = @. df[:, 2]
 
-plot(df[:,1], [df[:,2] voltage_input])
+time[112]
+time[247]
+
+time_ = time[112:247]
+volt_ = voltage_input[112:247]
+speed_ = speed[112:247]
+#plot(df[:,1], [df[:,2] voltage_input])
+plot(time_, [volt_ speed_])
 
